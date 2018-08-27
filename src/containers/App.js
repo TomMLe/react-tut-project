d314edcbbe649666aee830a5246aacb5f083c7ef
Tom Le <lmt1205@uw.edu>
2018-08-26 23:06:06 -0700

    hoc2

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends PureComponent {
   constructor(props) {
@@ -79,7 +80,7 @@ class App extends PureComponent {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes = {classes.App}>
         <button onClick={() => {this.setState({showPersons:true})}}> Show Persons </button>
 
         <Cockpit
@@ -89,7 +90,7 @@ class App extends PureComponent {
           clicked = {this.togglePersonsHandler}
         />
         {persons}
-      </div>
+      </WithClass>
     );
     // return React.createElement('div', {className:'App'}, React.createElement('h1',null,'I\'m Tom'));
   }
